726e8c8cf0041aaeedf2fa1c7529651112a8cdb0
Lego79 <kentcandoit89@gmail.com>
2023-08-17 22:48:52 +0900

    멤버레퍼지토리 v1 DELETE테스트

--- src/test/java/hello/jdbc/test/TEST.java
+++ src/test/java/hello/jdbc/test/TEST.java
@@ -44,4 +44,22 @@ class MemberRepositoryV1Test {
         assertThatThrownBy(() -> repository.findById(member.getMemberId()))
                 .isInstanceOf(NoSuchElementException.class);
     }
+    @Test
+    void crud() throws SQLException, InterruptedException {
+        log.info("start");
+        //save
+        Member member = new Member("memberV0", 10000);
+        repository.save(member);
+        //findById
+        Member memberById = repository.findById(member.getMemberId());
+        assertThat(memberById).isNotNull();
+        //update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+    }
 }
